fb88b54088e8439e5f9f4a2f3e96206ea624f338
Strigix <stefan@vtveld.nl>
2023-04-12 12:13:31 +0200

    Made changes to root.go for golint clearance, edited psp, cleaned up temp helm files.

--- root.go
+++ root.go
@@ -49,8 +49,18 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	goflag.Set("logtostderr", "true")
-	goflag.CommandLine.Parse([]string{})
+	err := goflag.Set("logtostderr", "true")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	err := goflag.CommandLine.Parse([]string{})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
 
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
